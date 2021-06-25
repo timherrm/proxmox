f2350135571f4828ba71585bcf29e8275fbdc8e7
timherrm <timherrm@users.noreply.github.com>
2021-06-25 19:28:39 +0200

    Update vm-setup-w-docker.sh

--- vm-setup-w-docker.sh
+++ vm-setup-w-docker.sh
@@ -29,7 +29,7 @@ mount /dockerdata
 lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 
 #portainer
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /dockerdata/portainer/data:/data portainer/portainer-ce
+docker run -d -p 9000:9000 --name=$HOSTNAME-portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /dockerdata/$HOSTNAME-portainer/data:/data portainer/portainer-ce
 
 #apt cleanup
 apt autoremove -y
