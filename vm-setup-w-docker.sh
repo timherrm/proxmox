d42179a936a9a4c962851bcedb01c16a767c0980
timherrm <timherrm@users.noreply.github.com>
2021-06-25 19:14:15 +0200

    Update vm-setup-w-docker.sh

--- vm-setup-w-docker.sh
+++ vm-setup-w-docker.sh
@@ -25,6 +25,9 @@ mkdir /dockerdata
 echo "10.0.40.186:/mnt/virtio1/$HOSTNAME /dockerdata nfs defaults 0 0" >> /etc/fstab
 mount /dockerdata
 
+#resize LV
+lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+
 #portainer
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /dockerdata/portainer/data:/data portainer/portainer-ce
 
